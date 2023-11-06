74d20f6eb1192b15f5c3f518dffddf204133c256
YummYume <yummyume@users.noreply.github.com>
2023-11-06 17:35:12 +0100

    Complete YAMS test

--- index.ts
+++ index.ts
@@ -3,5 +3,12 @@ export const getScoreForDices = (dices: number[]) => {
     throw new Error('Function getScoreForDices requires exactly 5 dices.');
   }
 
+  const sortedDices = dices.sort();
+
+  // YAMS
+  if (sortedDices[0] === sortedDices[4]) {
+    return 50;
+  }
+
   return dices.reduce((sum, dice) => sum + dice, 0);
 }
