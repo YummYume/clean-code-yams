065fcb442692e9a7247c09cae25411dc844f3445
YummYume <yummyume@users.noreply.github.com>
2023-11-06 18:24:23 +0100

    Implement throwDices tests

--- index.test.ts
+++ index.test.ts
@@ -39,3 +39,25 @@ describe('getScoreForDices returns correct score', () => {
     expect(score).toBe(30);
   });
 });
+
+describe.todo('throwDices returns correct score', () => {
+  const throwResults = [
+    { throws: [[1, 3, 5, 6, 2], [1, 3, 5, 6, 2], [1, 3, 5, 6, 2]], expectedScore: 51 },
+    // YAMS
+    { throws: [[1, 3, 5, 6, 2], [1, 3, 5, 6, 2], [1, 1, 1, 1, 1]], expectedScore: 84 },
+    // Grande suite
+    { throws: [[2, 3, 4, 5, 6], [2, 3, 4, 5, 6], [2, 3, 4, 5, 6]], expectedScore: 120 },
+    // Brelan
+    { throws: [[1, 1, 6, 2, 1], [1, 1, 6, 2, 1], [1, 1, 6, 2, 1]], expectedScore: 84 },
+    // Carré
+    { throws: [[1, 1, 1, 2, 1], [1, 1, 1, 2, 1], [1, 1, 1, 2, 1]], expectedScore: 105 },
+    // Full
+    { throws: [[2, 1, 1, 1, 2], [2, 1, 1, 1, 2], [2, 1, 1, 1, 2]], expectedScore: 90 },
+  ];
+
+  test.each(throwResults)('throwDices($throws) matches $expectedScore', ({ throws, expectedScore }) => {
+    // const score = throwDices(throws);
+
+    // expect(score).toBe(expectedScore);
+  });
+});
