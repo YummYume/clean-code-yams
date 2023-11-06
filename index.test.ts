a0b5993ac9a91ee1971441b3f9a2533ebd98fa03
YummYume <yummyume@users.noreply.github.com>
2023-11-06 17:30:20 +0100

    Implement first test

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,11 @@
-import { describe } from "bun:test";
+import { describe, expect, mock, test } from "bun:test";
 
-describe.todo('TODO', () => {
+const getScoreForDices = mock((dices: number[]) => 0);
+
+describe('getScoreForDices returns correct score', () => {
+  test('No figure should return sum of dices', () => {
+    const score = getScoreForDices([1, 2, 3, 4, 5]);
+
+    expect(score).toBe(15);
+  });
 });
