8e6ef115281cd250d56b4a610fcc14716730634e
YummYume <yummyume@users.noreply.github.com>
2023-11-06 17:53:11 +0100

    Complete Grande Suite test + use constants

--- index.test.ts
+++ index.test.ts
@@ -4,9 +4,9 @@ import { getScoreForDices } from ".";
 
 describe('getScoreForDices returns correct score', () => {
   test('No figure should return sum of dices', () => {
-    const score = getScoreForDices([1, 2, 3, 4, 5]);
+    const score = getScoreForDices([1, 3, 5, 7, 9]);
 
-    expect(score).toBe(15);
+    expect(score).toBe(25);
   });
 
   test('YAMS should return 50', () => {
--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
+const YAMS = 50 as const;
+const GRANDE_SUITE = 40 as const;
+
+const isYams = (dices: number[]) => dices[0] === dices[4];
+
+const isGrandeSuite = (dices: number[]) => {
+  let previousDiceResult = dices[0];
+
+  for (const dice of dices.slice(1)) {
+    if (dice !== previousDiceResult + 1) {
+      return false;
+    }
+
+    previousDiceResult = dice;
+  }
+
+  return true;
+};
+
 export const getScoreForDices = (dices: number[]) => {
   if (dices.length !== 5) {
     throw new Error('Function getScoreForDices requires exactly 5 dices.');
@@ -6,8 +25,13 @@ export const getScoreForDices = (dices: number[]) => {
   const sortedDices = dices.sort();
 
   // YAMS
-  if (sortedDices[0] === sortedDices[4]) {
-    return 50;
+  if (isYams(sortedDices)) {
+    return YAMS;
+  }
+
+  // Grande suite
+  if (isGrandeSuite(sortedDices)) {
+    return GRANDE_SUITE;
   }
 
   return dices.reduce((sum, dice) => sum + dice, 0);
